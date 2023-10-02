c196f63b35070fd259b639dffa117fbb97f972ec
nikhil vipradas <nikhilvipradas@gmail.com>
2023-10-02 17:23:16 +0530

    Added sample.java file

--- NXPWidget/src/com/velocity/Sample.java
+++ NXPWidget/src/com/velocity/Sample.java
@@ -1,15 +1,12 @@
 package com.velocity;
 
-public class Demo {
+public class Sample {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-		
-		for (int i=0;i<=5;i++) {
-			System.out.println("Value of i>>"+i);
-		}
 
+		System.out.println("This is Sample Java File");
+		
 	}
 
 }
